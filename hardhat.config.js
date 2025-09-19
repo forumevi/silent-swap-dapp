abf4fbb9504288447fc406ae7550fab952cefcb8
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-20 00:17:46 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: "ALCHEMY_RPC_URL_HERE",
-      accounts: ["PRIVATE_KEY_HERE"],
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
